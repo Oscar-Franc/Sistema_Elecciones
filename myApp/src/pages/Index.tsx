97e4699c68fb6eac128b2f0e74a247ab93b5d91b
juan-hereiva <hereivajuan@gmail.com>
2024-11-05 08:38:10 -0600

    Cambios del 5/11/2024

--- myApp/src/pages/Index.tsx
+++ myApp/src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import { IonModal, IonImg, IonIcon, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonModal, IonImg, IonIcon, IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCheckbox,IonLabel, IonInput   } from '@ionic/react';
 import React, { useState } from 'react';
 import { alertCircleOutline } from 'ionicons/icons'; 
 import '../theme/variables.css'
@@ -6,10 +6,16 @@ import candidataImage from '../../assets/img/candidata.jpg';
 import candidataImage2 from  '../../assets/img/candidata3.jpg'
 import candidatoImage from '../../assets/img/hombreCandidato.jpg'
 import UaemexImage from '../../assets/img/Logo_de_la_UAEMex.svg';
+
+
+
 const Index: React.FC = () => {
     
     const [isModalOpen, setIsModalOpen] = useState(false);
-
+    const [inputValue, setInputValue] = useState('');
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault(); 
+    }
     return (
         <IonPage >
              <IonHeader class='none-shadow'>
@@ -28,55 +34,64 @@ const Index: React.FC = () => {
     
             <IonContent className="ion-padding custom-content"    >
                 <h3 className='sin-estilos '>Bienvenido:</h3>
-                <p  className='sin-estilos estilo-texto'>Elige a tu candidato</p>
+                <p  className='sin-estilos estilo-texto'>Elige a tu candidato. Esta es una prueba piloto</p>
                 <div className='contenedor-candidatos'  >
                  <form >
                     
                     <div className='contenedor-candidato '>
-                        <IonButton expand="block" className='info-icon' routerLink='/infoCandidatos' ><IonIcon className='estilo-icono' icon={alertCircleOutline} /></IonButton>
+                        
                         <div className='info-contenedor'>
+                        <IonCheckbox/>
                         <img src={candidataImage} alt="Candidata" />
                         <div className='fondo-info'>
                             <p className='sin-estilos '  >Nombre: Maria Luisa Hernandez Sanchez</p>
                             <p className='sin-estilos '>Edad:  40 años</p>
                             <p className='sin-estilos '>Tiempo en la universidad: 25 años</p>
-                            <p className='sin-estilos '>Cargo actual: Docente</p>
+                            <p className='sin-estilos '>Grado Academico: Maestria</p>
                         </div>
                         
                         </div>
                         
-                        <IonButton fill="clear" color={'verde1'} className=' estilo-letras-login' expand='full' type='submit'>Candidato 1</IonButton>
+                        
                     </div>
 
                     <div className='contenedor-candidato '>
-                        <IonButton expand="block" routerLink='/infoCandidatos' className='info-icon' ><IonIcon className='estilo-icono' icon={alertCircleOutline} /></IonButton>
+                        
                         <div className='info-contenedor'>
+                        <IonCheckbox/>
                         <img src={candidataImage2} alt="Candidata" />
                         <div className='fondo-info'>
                             <p className='sin-estilos '  >Nombre: Samara FLores Alba</p>
                             <p className='sin-estilos '>Edad: 50 años</p>
                             <p className='sin-estilos '>Tiempo en la universidad: 28 años</p>
-                            <p className='sin-estilos '>Cargo actual: Docente</p>
+                            <p className='sin-estilos '>Grado Academico: Doctorado</p>
                         </div>
                         
                         </div>
-                        <IonButton fill="clear" color={'verde1'} className='estilo-letras-login' expand='full' type='submit'>Candidato 2</IonButton>
+                        
                     </div>
 
                     <div className='contenedor-candidato  '>
-                        <IonButton  expand="block" routerLink='/infoCandidatos' className='info-icon' ><IonIcon className='estilo-icono'  icon={alertCircleOutline} /></IonButton>
                         <div className='info-contenedor'> 
+                        <IonCheckbox/>
                         <img src={candidatoImage} alt="Candidata" />
                         <div className='fondo-info'>
                             <p className='sin-estilos '  >Nombre: Victor Hugo Lopez</p>
                             <p className='sin-estilos '>Edad: 55 años</p>
                             <p className='sin-estilos '>Tiempo en la universidad: 30 años</p>
-                            <p className='sin-estilos '>Cargo actual: Docente</p>
+                            <p className='sin-estilos '>Grado Academico: Doctorado</p>
                         </div>
                         
                         </div>
-                        <IonButton fill="clear" color={'verde1'} className=' estilo-letras-login' expand='full' type='submit'>Candidato 3</IonButton>
+                        
                     </div>
+                    <br />
+                    <IonLabel position="floating">Escribe el nombre de algun otro candidato</IonLabel>
+                    <br />
+                    <IonInput className='caja-login' fill='outline' labelPlacement='floating' type='text' placeholder='Luis Hernadez Sanchez' label='Otro'  ></IonInput>
+                     <div>
+                     <IonButton fill="clear" color={'verde1'} className=' estilo-letras-login' expand='full' type='submit'>Votar</IonButton>
+                     </div>
                     <div>
                     <IonButton fill="clear" color={'verde1'} className='ion-margin-top estilo-letras-login' expand='full' type='submit'>Anular Voto</IonButton>
                     </div>
--- myApp/src/pages/Login.tsx
+++ myApp/src/pages/Login.tsx
@@ -13,7 +13,7 @@ const Login: React.FC = () => {
     const handleLogin = async (e: React.FormEvent) => {
         e.preventDefault(); 
         try {
-            const response = await fetch('http://192.168.1.8:3000/login', {
+            const response = await fetch('http://172.26.53.195:3000/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -46,7 +46,7 @@ const Login: React.FC = () => {
                     <img src={UaemexImage} className='img2' alt="logoUaemex" />
                     </div>
                     
-                    <IonTitle color={'oroVerde'} className="estilo-letras" >Universidad Autonoma <br></br> del Estado de Mexico</IonTitle>
+                    <IonTitle color={'oroVerde'} className="estilo-letras" >Universidad Autonoma del <br />Estado de Mexico <br /> Facultad de Ingenieria</IonTitle>
                 </div>
                 
             
@@ -59,14 +59,16 @@ const Login: React.FC = () => {
             <IonContent >
             
             <div className='page-background '>
-                <h1 className='sin-bordes estilo-titulo'>Urna
-                 Electronica Electoral</h1>
+                <h1 className='sin-bordes estilo-titulo'>Prueba de Urna
+                 Electronica Electoral  </h1>
                     <IonCard  className="sin-bordes  ">
                         <IonCardContent >
                          <form onSubmit={handleLogin} >
                             <IonInput className='caja-login' fill='outline' labelPlacement='floating' value={email} type='email' placeholder='ejemplo@alumno.uaemex.mx' label='Correo Escolar' onIonChange={(e) => setEmail(e.detail.value!)} ></IonInput>
                             <IonInput className='caja-login ion-margin-top' fill='outline' value={password} labelPlacement='floating' type='password' placeholder='1711890' onIonChange={(e) => setPassword(e.detail.value!)} label='Numero de Cuenta'></IonInput>
                             <IonButton fill="clear" color={'verde1'} className='ion-margin-top estilo-letras-login' expand='full' type='submit'>Login</IonButton>
+                            
+                            
                             {/* Colocar la imagen <IonButton routerLink='/register' fill="clear" color={'oro1'} className='ion-margin-top'  type='submit' expand='full'  >Create account</IonButton> */}
                         </form>
                         </IonCardContent>
